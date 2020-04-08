84f77e0b8d2d4dd49c62bb933ea2e2e0105d15bf
Yeasha Hassan <tiram819@gmail.com>
2020-04-08 04:54:03 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,13 @@
+counter = 0
+
+loop do
+  puts "Never pass a school bus if it has flashing red lights"
+end
+
+toppings = ["pickles", "mushrooms", "bacon"]
+ 
+def hamburger(toppings)
+  toppings.each do |topping|
+    puts "I love #{topping} on my burgers!"
+  end
+end
